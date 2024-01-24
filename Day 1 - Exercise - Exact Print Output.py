026695a21634412b50c704db40fc9129c550b7b7
COMJackson <150160673+COMJackson@users.noreply.github.com>
2024-01-24 12:05:00 -0500

    Add files via upload

---
+++ Day 1 - Exercise - Exact Print Output.py
@@ -0,0 +1,7 @@
+# The purpose of this exercise was to define exactly what the print function would output to console.
+
+print("Day 1 - Python Print Function"
+      + "\n"
+      + "The function is declared like this:"
+      + "\n"
+      + "print('what to print')")
